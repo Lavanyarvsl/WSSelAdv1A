679f67fef572f0ec464393abd4b91119be70e1bb
lavanyais041 <lavanyarvis041@gmail.com>
2018-02-24 10:13:10 +0530

    My First Commit

---
+++ Run.bat
@@ -0,0 +1,1 @@
+java -cp bin qsp.DemoA &pause
---
+++ src/qsp/DemoA.java
@@ -0,0 +1,12 @@
+package qsp;
+//27-01-2018 Jenkins Java project
+public class DemoA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello");
+		
+	}
+
+}
